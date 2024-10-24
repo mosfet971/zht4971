2bd293dbffec4612af3ce49eab9baddbb5e484c8
mosfet971 <mosfet971@gmail.com>
2024-10-24 23:41:19 +0500

    instruction tab

--- src/frontend/components/specific/pages/MainPage.jsx
+++ src/frontend/components/specific/pages/MainPage.jsx
@@ -12,6 +12,7 @@ import TabMenu from "../../universal/TabMenu.jsx";
 import TabButton from "../../universal/TabButton.jsx";
 import TabPanel from "../../universal/TabPanel.jsx";
 import ListTab from "../tabs/ListTab.jsx";
+import InfoTab from "../tabs/InfoTab.jsx";
 
 
 let MainPage = observer(() => {
@@ -21,16 +22,14 @@ let MainPage = observer(() => {
         <TabButton target="menu" isDefaultSelected={true}>Меню</TabButton>
         <TabButton target="list" isDefaultSelected={false}>Список</TabButton>
         <TabButton target="primary" isDefaultSelected={false}>Избранное</TabButton>
-        <TabButton target="recent" isDefaultSelected={false}>Недавнее</TabButton>
-        <TabButton target="search" isDefaultSelected={false}>Поиск</TabButton>
+        <TabButton target="info" isDefaultSelected={false}>Инструкция</TabButton>
         <TabButton target="readAndWrite" isDefaultSelected={false}>Просмотр и изменение записи</TabButton>
       </TabMenu>
       <hr style={{margin: 0}}/>
       <TabPanel isDefaultHidden={false} id="menu"><MenuTab/></TabPanel>
       <TabPanel isDefaultHidden={true} id="list"><ListTab/></TabPanel>
       <TabPanel isDefaultHidden={true} id="primary"><PrimaryTab/></TabPanel>
-      <TabPanel isDefaultHidden={true} id="recent">Tab B3 is active</TabPanel>
-      <TabPanel isDefaultHidden={true} id="search">Tab B4 is active</TabPanel>
+      <TabPanel isDefaultHidden={true} id="info"><InfoTab/></TabPanel>
       <TabPanel isDefaultHidden={true} id="readAndWrite"><NoteTab/></TabPanel>
     </TabSection>
   </>);
